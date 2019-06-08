ef6671bdde16812a7156eca0ded1ae47a3dc2c44
Matheus Ochi <matheus@revelare.com.br>
2019-06-08 16:48:23 -0300

    Base do projeto com implantação do MD e módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: '@pages/home/home.module#HomeModule'
+  },
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '@core/modules/material.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MaterialModule
+  ],
+  declarations: [
+    HomeComponent
+  ]
+})
+export class HomeModule {
+}
