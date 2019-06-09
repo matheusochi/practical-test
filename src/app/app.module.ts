d8c391758da897c720bbb873ec52ce9ac5aec159
Matheus Ochi <matheus@revelare.com.br>
2019-06-08 21:16:32 -0300

    Página de busca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MaterialModule } from '@core/modules/material.module';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -12,8 +14,10 @@ import { MaterialModule } from '@core/modules/material.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@core/modules/material.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome'
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MaterialModule
+    MaterialModule,
+    AngularFontAwesomeModule
   ],
   declarations: [
     HomeComponent
